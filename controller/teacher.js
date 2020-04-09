65c5b9b6eaf4a046cc8e6e9d548bb289305ffc06
Abhinav Mani <abhinav1946@gmail.com>
2020-04-09 18:54:03 +0530

    marks entry form

--- controller/teacher.js
+++ controller/teacher.js
@@ -12,3 +12,16 @@ module.exports.GET_HOME=(req,res)=>{
         }
     }
 }
+
+module.exports.GET_STUDENT_MARKS_ENTRY=(req,res)=>{
+    showMarksEntryForm()
+    async function showMarksEntryForm(){
+        const code =  req.params.code;
+        let a=[]
+        for(let i=0;i<8;i++){
+            a.push(code);
+        }
+        const [students,extra]=await pool.query("select rollno from cousrechoice where subject1=? OR subject2=? OR subject3=? OR subject4=? OR subject5=? OR subject6=? OR subject7=? OR subject8=?",a);
+        res.render("marksentry",{title:"Teacher",students:students});
+    }
+}
--- routes/teacher.js
+++ routes/teacher.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const teacherController = require("../controller/teacher");
 
 router.get("/",teacherController.GET_HOME);
+router.get("/:code",teacherController.GET_STUDENT_MARKS_ENTRY);
 
 
 module.exports = router
